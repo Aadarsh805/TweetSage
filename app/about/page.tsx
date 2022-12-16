ec451f526a904d41389e05e312cdddffc3b6dc38
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-16 06:11:44 +0530

    minor changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,107 +1,131 @@
 "use client";
 
-import React, { useEffect, useState } from 'react'
-import TextField from '@mui/material/TextField';
-import { ColorRing } from 'react-loader-spinner'
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import copy from "copy-to-clipboard"
+import React, { FC, useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import { ColorRing } from "react-loader-spinner";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import copy from "copy-to-clipboard";
+import Link from "next/link";
 
+const exampleQuestion = [
+  "Tell me about the person",
+  "Tell me 5 jokes on that person",
+  "How will u describe the person above",
+  "Tell something about the person in a sentence",
+];
 const Page = () => {
+  const [loading, setLoading] = useState(false);
+  const [copyText, setCopyText] = useState("");
 
-    const exampleQuestion = [
-        "Tell me about the person",
-        "Tell me 5 jokes on that person",
-        "How will u describe the person above",
-        "Tell something about the person in a sentence"
+  const handleClick = () => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      console.log("Loading", loading);
+      setLoading(false);
+    }, 2000);
 
-    ]
+    return () => clearTimeout(timer);
+  };
 
-    const [loading, setLoading] = useState(false)
-    const [ copyText, setCopyText ] = useState("")
-
-    const handleClick = () => {
-        setLoading(true)
-        const timer = setTimeout(() => {
-            console.log("Loading", loading)
-            setLoading(false)
-        }, 2000)
-
-        return () => clearTimeout(timer)
-
-    }
-
-
-
-    const Examples = () => {
-        return (
-            exampleQuestion.map((item, index) => (
-                <div key={index} className='flex items-center justify-between bg-gray-100 mb-5 py-2 px-3'>
-                    <p>{item}</p>
-                    <div onClick={navigator.clipboard.writeText(item)}>
-                        <ContentPasteIcon className='cursor-pointer' />
-                    </div>
-                </div>
-            ))
-        )
-    }
-
-    return (
-        <div className='bg-[#E8D7F1] w-full h-full flex flex-col items-center  mx-auto' >
-
-            <div className='text-center my-20 flex flex-col item-center gap-2'>
-                {/* <h4 className='font-bold text-[#0EA5E9] text-xl tracking-wider'>Tweet Sage</h4> */}
-                <p className='text-3xl sm:text-4xl lg:text-5xl font-semibold  w-[13em] sm:w-[14em] md:w-[15em] lg:w-[17em] text-[#932F6D]'>Why Tweet Sage? <br /> Because your tweets deserve some sage advice.</p>
-            </div>
-            <div className='flex flex-col-reverse items-center justify-center'>
-                <div className='text-center flex flex-col items-center justify-center w-[20em]'>
-                    <p className='text-xl sm:text-2xl mb-8 font-semibold'>What do you want to know about the person?</p>
-                    <div className='flex flex-col  item-center gap-2 justify-center w-full'>
-                        <TextField id="outlined-basic" label="Username" variant="outlined" />
-                        <button onClick={handleClick} className='bg-[#0EA5E9] py-2 px-10 font-bold text-white'>Get Tweets</button>
-                        {loading && <ColorRing
-                            visible={true}
-                            height="80"
-                            width="80"
-                            ariaLabel="blocks-loading"
-                            wrapperStyle={{}}
-                            wrapperClass="blocks-wrapper"
-                            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-                        />}
-                    </div>
-                    <div className='mt-5 flex flex-col w-full gap-2'>
-                        <TextField id="outlined-basic" variant="outlined" placeholder='Enter your question' />
-                        <button className='bg-[#0EA5E9] mr-10 w-full py-2 text-white font-bold'>Get Data</button>
-                    </div>
-                </div>
-                <a href="/start"><button className='bg-[#0EA5E9] py-3 px-5 mt-20'>Go to start</button></a>
-
-                <div>
-                    <h2 className='font-semibold text-lg'>Try these questions:</h2>
-                    <div className='bg-white w-[20em] h-full mb-10 pt-3 pb-2 px-4'>
-                        <div className='flex items-center justify-between  w-full  '>
-                            <div className='flex items-center justify-center gap-2'>
-                                <span className='w-4 h-4 rounded-full bg-[#C884A6]'></span>
-                                <span className='w-4 h-4 rounded-full bg-[#FFE74C]'></span>
-                                <span className='w-4 h-4 rounded-full bg-[#81F495]'></span>
-                            </div>
-
-                            <h1 className='bg-[#0EA5E9] text-white font-bold py-1.5 px-3 rounded-full '>Tweet Sage</h1>
-                        </div>
-
-                        <div className=' mt-2'>
-                            <Examples />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-[#E8D7F1] w-full h-full flex flex-col items-center  mx-auto">
+      <div className="text-center my-20 flex flex-col item-center gap-2">
+        {/* <h4 className='font-bold text-[#0EA5E9] text-xl tracking-wider'>Tweet Sage</h4> */}
+        <p className="text-3xl sm:text-4xl lg:text-5xl font-semibold  w-[13em] sm:w-[14em] md:w-[15em] lg:w-[17em] text-[#932F6D]">
+          Tweet Sage: Because your tweets deserve some sage advice.
+        </p>
+      </div>
+      <div className="flex flex-col-reverse items-center justify-center">
+        <div className="text-center flex flex-col items-center justify-center w-[20em]">
+          <p className="text-xl sm:text-2xl mb-8 font-semibold">
+            What do you want to know about the person?
+          </p>
+          <div className="flex flex-col  item-center gap-2 justify-center w-full">
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+            />
+            <button
+              onClick={handleClick}
+              className="bg-[#0EA5E9] py-2 px-10 font-bold text-white"
+            >
+              Get Tweets
+            </button>
+            {loading && (
+              <ColorRing
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+              />
+            )}
+          </div>
+          <div className="mt-5 flex flex-col w-full gap-2">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              placeholder="Enter your question"
+            />
+            <button className="bg-[#0EA5E9] mr-10 w-full py-2 text-white font-bold">
+              Get Data
+            </button>
+          </div>
         </div>
-    )
-}
+        <Link href="/start">
+          <button className="bg-[#0EA5E9] py-3 px-5 mt-20">Go to start</button>
+        </Link>
 
-export default Page
+        <div>
+          <h2 className="font-semibold text-lg">Try these questions:</h2>
+          <div className="bg-white w-[20em] h-full mb-10 pt-3 pb-2 px-4">
+            <div className="flex items-center gap-2 mb-4">
+              <span className="w-4 h-4 rounded-full bg-[#C884A6]"></span>
+              <span className="w-4 h-4 rounded-full bg-[#FFE74C]"></span>
+              <span className="w-4 h-4 rounded-full bg-[#81F495]"></span>
+            </div>
+
+            <div className=" mt-2">
+              <Examples />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
 
 // 1. Tell me about the person
 // 2. Tell me 5 jokes on that person
-// 3. How will u describe the person above 
-// 4. Tell 5 good points on this person 
-// 5. Tell something about the person in a sentence 
+// 3. How will u describe the person above
+// 4. Tell 5 good points on this person
+// 5. Tell something about the person in a sentence
+
+const Examples: FC = () => {
+  return (
+    <div>
+      {exampleQuestion.map((item, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-between bg-gray-100 mb-5 py-2 px-3"
+        >
+          <p>{item}</p>
+          <div
+            className="relative group"
+            onClick={() => navigator.clipboard.writeText(item)}
+          >
+            <span className="copied z-20 rounded-md absolute p-1 -top-8 -right-10 bg-black text-white text-xs hidden group-active:block">
+              copied
+            </span>
+            <ContentPasteIcon className="cursor-pointer" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,28 +1,38 @@
-
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 const page = () => {
-    return (
-
-        <div className="relative  overflow-hidden bg-[#FEFCF3]">
-            <nav className='absolute w-full flex items-center justify-between text-white p-10'>
-                <div className='flex items-center justify-center gap-5'>
-                    {/* <span className='bg-[red] w-10'>logo</span> */}
-                    <h2 className='font-bold text-2xl tracking-wider text-[#0EA5E9]'>Tweet Sage</h2>
-                </div>
-                <div>
-                    <span className='text-black'>Github Link</span>
-                </div>
-            </nav>
-
-            <div className='z-10 text-black flex flex-col items-center justify-center h-screen w-[78em] mx-auto text-center gap-6 lg:w-[78em] md:w-[60em] sm:w-[50em]'>
-                <h1 className='font-extrabold text-7xl leading-[1em]'>Where even your most ridiculous tweets can reveal hidden truths </h1>
-                <p className='text-slate-400 w-[44em] text-xl'>A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup. </p>
-                <a href="/about"><button className='bg-[#0EA5E9] py-2 px-5 rounded-md font-semibold '>Get Started</button></a>
-                {/* <button className='bg-[#0EA5E9] py-2 px-5 rounded-md font-semibold '>Get Started</button> */}
-            </div>
-
+  return (
+    <div className="relative  overflow-hidden bg-[#FEFCF3]">
+      <nav className="absolute w-full flex items-center justify-between text-white p-10">
+        <div className="flex items-center justify-center gap-5">
+          {/* <span className='bg-[red] w-10'>logo</span> */}
+          <h2 className="font-bold text-2xl tracking-wider text-[#0EA5E9]">
+            Tweet Sage
+          </h2>
         </div>
-    )
-}
+        <div>
+          <span className="text-black">Github Link</span>
+        </div>
+      </nav>
 
-export default page
+      <div className="z-10 text-black flex flex-col items-center justify-center h-screen w-[78em] mx-auto text-center gap-6 lg:w-[78em] md:w-[60em] sm:w-[50em]">
+        <h1 className="font-extrabold text-7xl leading-[1em]">
+          Where even your most ridiculous tweets can reveal hidden truths{" "}
+        </h1>
+        <p className="text-slate-400 w-[44em] text-xl">
+          A utility-first CSS framework packed with classes like flex, pt-4,
+          text-center and rotate-90 that can be composed to build any design,
+          directly in your markup.{" "}
+        </p>
+        <Link href="/about">
+          <button className="bg-[#0EA5E9] py-2 px-5 rounded-md font-semibold ">
+            Get Started
+          </button>
+        </Link>
+        {/* <button className='bg-[#0EA5E9] py-2 px-5 rounded-md font-semibold '>Get Started</button> */}
+      </div>
+    </div>
+  );
+};
+
+export default page;
