a702cac3b2f2eefac8be46a0a4cb67fd27f18ee2
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-16 22:37:36 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import "../styles/globals.css";
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import Navbar from "./Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const theme = createTheme({
   palette: {
     action: {
       disabledBackground: "red",
       disabled: "white",
-      
     },
   },
 });
@@ -23,12 +23,12 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="bg-[#EFF5F5]" >
-      <Navbar />
-      <div className="mt-[3.2rem] h-screen">
-
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </div>
+      <body className="bg-[#EFF5F5]">
+        <Navbar />
+        <div className="mt-[3.2rem] h-screen">
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </div>
+        <Analytics />
       </body>
     </html>
   );
