c62584b80ecbe2a595351284ba97674f60dd011c
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-17 17:26:27 +0530

    viewport fix

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,7 @@ export default function Head() {
         content="Get personalized answers to your questions based on your recent tweets. Our website analyzes your Twitter activity to provide you with accurate and relevant responses to your queries."
       />
       <meta name="author" content="Aadarsh Thakur" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
