8315be2bf78d9498e211d93aff87a30e5e6894a8
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-18 18:27:48 +0530

    examples div visibility chagne on smaller screen

--- app/play/Example.tsx
+++ app/play/Example.tsx
@@ -13,12 +13,9 @@ const exampleQuestion = [
 
 const Example = () => {
   return (
-    <div className="2xl:flex hidden lg:gap-20  items-center justify-center lg:absolute right-[5em] md:top-1/2 lg:-translate-y-[18em] z-30">
+    <div className="lg:flex hidden lg:gap-20  items-center justify-center lg:absolute xl:right-[5em] right-0 md:top-1/2 lg:-translate-y-[18em] z-30">
       <div className="">
-        {/* <h2 className="font-semibold text-lg text-center mb-2">
-          Try these questions:
-        </h2> */}
-        <div className="example bg-white lg:shadow-lg shadow-md w-[20em] 2xl:w-[22em] max-h-[10em] 2xl:max-h-fit mb-10 pt-3 pb-1 px-4  overflow-scroll lg:h-full rounded-lg">
+        <div className="example bg-white lg:shadow-lg shadow-md w-[14em] 2xl:w-[22em] max-h-[14em] 2xl:max-h-fit mb-10 pt-3 pb-1 xl:px-4 px-2 overflow-scroll lg:h-full rounded-lg">
           {/* <div className="flex items-center gap-2 mb-4 ml-1">
             <span className="w-3 h-3 rounded-full bg-[#C884A6]"></span>
             <span className="w-3 h-3 rounded-full bg-[#FFE74C]"></span>
@@ -47,7 +44,7 @@ const Examples: FC = () => {
           key={index}
           className="flex justify-between bg-[#f8f9fd] rounded-md mb-2 py-2 px-3"
         >
-          <p>{item}</p>
+          <p className='text-base 2xl:text-lg' >{item}</p>
           <div onClick={() => navigator.clipboard.writeText(item)}>
             <Tooltip title="copy">
               <ContentCopyIcon className="cursor-pointer w-3" />
