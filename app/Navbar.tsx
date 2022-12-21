5c43fa18a7086a05b5f2c2ada29a6e1e5eb4563c
steve <thuqpalmer@gmail.com>
2022-12-21 22:12:15 +0000

    [add]:login with twitter btn

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import Logo from "../public/logo2.png";
 import Image from "next/image";
 
@@ -18,12 +19,18 @@ const Navbar = () => {
         href="https://github.com/Aadarsh805/TweetSage.ai"
         className="outline-none noOutline-btn"
       >
+        <div className="flex justify-center">
+            <button className="hidden items-center mx-2 rounded-md bg-blue-500 p-2 font-medium lg:flex">
+          <TwitterIcon/>
+          Login with Twitter
+        </button>
         <div className="gap-2 inline-flex items-center justify-center h-10 px-2 py-3 text-xs bg-transparent text-white border-2 border-[#0904268e] rounded-md outline-none">
           <GitHubIcon className="text-xl sm:text-2xl text-[#090426db] w-7 h-7" />
           <span className="h-6 w-[1px] bg-[#090426ad]"></span>
           <span className="uppercase font-semibold sm:text-sm text-[#090426] text-xs">
             Star us
           </span>
+        </div>
         </div>
       </Link>
     </nav>
